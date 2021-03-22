681091d36d85877ee1fa5253ef35a979f6f96fe9
Mustafa Ömer ESER <mustafaomereser@gmail.com>
2021-03-23 02:28:21 +0300

    added helper

---
+++ app/Http/Controllers/Homee/home.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Homee;
+
+class home
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update($id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
